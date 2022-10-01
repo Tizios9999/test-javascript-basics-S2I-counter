4bd8556002522faa3b83251657d55d4f2cab969c
Tizios9999 <davide.santonocito@outlook.com>
2022-10-01 11:42:09 +0200

    js completed

--- counter.js
+++ counter.js
@@ -1,1 +1,36 @@
-// Here I will insert the JS code
+// Initial counter setup
+let counter;
+const counterElement = document.querySelector(".number");
+const numberLimit = 99;
+
+function counterUpdate(element) {
+    element.textContent = counter;
+}
+
+function counterInit() {
+    counter = 0;
+    counterUpdate(counterElement);
+}
+
+counterInit();
+
+// Button click events
+
+btnSection = document.querySelector(".btn-section");
+minusBtn = document.querySelector(".minus");
+plusBtn = document.querySelector(".plus");
+
+btnSection.addEventListener("click", function(e) {
+    e.target.classList.contains("plus") ? modifyCounter(1) : 
+    e.target.classList.contains("minus") ? modifyCounter(-1): 
+    e.target.classList.contains("reset") ? counterInit() :
+    e.stopPropagation();
+});
+function modifyCounter(number) {
+    counter+= number;
+    if (Math.abs(counter) > numberLimit) { 
+        counterInit(); 
+    } else {
+        counterUpdate(counterElement);
+    }
+}
