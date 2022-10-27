d6e114802acb831eb0ff3c198116ba8ac059170d
Tizios9999 <davide.santonocito@outlook.com>
2022-10-27 16:14:19 +0200

    create elements with JS setup

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,7 +1,32 @@
+// Draw elements
+
+const headerElement = document.createElement('header');
+const h1Element = document.createElement('h1');
+const h1TextNode = document.createTextNode('A simple JS counter');
+const counterWrapperElement = document.createElement('div');
+counterWrapperElement.classList.add('counter-wrapper');
+const numberElement = document.createElement('span');
+numberElement.classList.add('number');
+
+const btnWrapper = document.createElement('div');
+btnWrapper.classList.add('btn-wrapper');
+
+const btnPlus = document.createElement('span');
+btnPlus.classList.add('counter-btn', 'plus');
+
+const btnMinus = document.createElement('span');
+btnMinus.classList.add('counter-btn', 'minus');
+
+const btnReset = document.createElement('span');
+btnReset.classList.add('counter-btn', 'reset');
+
+// Append elements
+
+
 // Initial counter setup
 let counter;
 const counterElement = document.querySelector(".number");
-const numberLimit = 99; // After reaching the number limit, the counter will restart from 0
+const NUMBER_LIMIT = 99; // After reaching the number limit, the counter will restart from 0
 
 function counterUpdate(element) {
     element.textContent = counter;
@@ -14,7 +39,7 @@ function counterInit() {
 
 function modifyCounter(number) {
     counter+= number;
-    if (Math.abs(counter) > numberLimit) { 
+    if (Math.abs(counter) > NUMBER_LIMIT) { 
         counterInit(); 
     } else {
         counterUpdate(counterElement);
