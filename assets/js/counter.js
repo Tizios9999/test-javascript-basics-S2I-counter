15cb843c1d5ee69f85d509c7e062aa1b6058674b
Tizios9999 <davide.santonocito@outlook.com>
2022-10-30 14:00:10 +0100

    added comments

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,75 +1,93 @@
-// Draw elements
+// Functions declarations
 
-const headerElement = document.createElement('header');
-const h1Element = document.createElement('h1');
-const h1TextNode = document.createTextNode('A simple JS counter');
-const mainSection = document.createElement('main');
-const counterWrapperElement = document.createElement('div');
-counterWrapperElement.classList.add('counter-wrapper');
-const numberElement = document.createElement('span');
-numberElement.classList.add('number');
+function renderPage() {
 
-const btnWrapper = document.createElement('div');
-btnWrapper.classList.add('btn-wrapper');
-
-const btnPlus = document.createElement('div');
-btnPlus.classList.add('counter-btn', 'plus');
-const plusTextNode = document.createTextNode('+');
-
-const btnMinus = document.createElement('div');
-btnMinus.classList.add('counter-btn', 'minus');
-const minusTextNode = document.createTextNode('-');
-
-const btnReset = document.createElement('div');
-btnReset.classList.add('counter-btn', 'reset');
-const resetTextNode = document.createTextNode('Reset');
-
-
-
-// Append elements
-
-const appSection = document.querySelector(".main-app");
-
-appSection.appendChild(headerElement);
-headerElement.appendChild(h1Element);
-h1Element.appendChild(h1TextNode);
-appSection.appendChild(mainSection);
-mainSection.appendChild(counterWrapperElement);
-counterWrapperElement.appendChild(numberElement);
-mainSection.appendChild(btnWrapper);
-btnWrapper.appendChild(btnMinus).appendChild(minusTextNode);
-btnWrapper.appendChild(btnPlus).appendChild(plusTextNode);
-btnWrapper.appendChild(btnReset).appendChild(resetTextNode);
-
-// Initial counter setup
-let counter;
-const NUMBER_LIMIT = 99; // After reaching the number limit, the counter will restart from 0
-
-function counterUpdate(element) {
+    const headerElement = document.createElement('header');
+    const h1Element = document.createElement('h1');
+    const h1TextNode = document.createTextNode('A simple JS counter');
+    const mainSection = document.createElement('main');
+    const counterWrapperElement = document.createElement('div');
+    counterWrapperElement.classList.add('counter-wrapper');
+    const numberElement = document.createElement('span');
+    numberElement.classList.add('number');
+    
+    
+    const btnWrapper = document.createElement('div');
+    btnWrapper.classList.add('btn-wrapper');
+    
+    const btnPlus = document.createElement('div');
+    btnPlus.classList.add('counter-btn', 'plus');
+    const plusTextNode = document.createTextNode('+');
+    
+    const btnMinus = document.createElement('div');
+    btnMinus.classList.add('counter-btn', 'minus');
+    const minusTextNode = document.createTextNode('-');
+    
+    const btnReset = document.createElement('div');
+    btnReset.classList.add('counter-btn', 'reset');
+    const resetTextNode = document.createTextNode('Reset');
+    
+    
+    
+    // Append elements
+    
+    const appSection = document.querySelector(".main-app");
+    
+    appSection.appendChild(headerElement);
+    headerElement.appendChild(h1Element);
+    h1Element.appendChild(h1TextNode);
+    appSection.appendChild(mainSection);
+    mainSection.appendChild(counterWrapperElement);
+    counterWrapperElement.appendChild(numberElement);
+    mainSection.appendChild(btnWrapper);
+    btnWrapper.appendChild(btnMinus).appendChild(minusTextNode);
+    btnWrapper.appendChild(btnPlus).appendChild(plusTextNode);
+    btnWrapper.appendChild(btnReset).appendChild(resetTextNode);
+    
+    
+}
+    
+function updateCounter(element) {
     element.textContent = counter;
 }
 
-function counterInit() {
+function initializeCounter() {
     counter = 0;
-    counterUpdate(numberElement);
+    updateCounter(numberElement);
 }
 
 function modifyCounter(number) {
     counter+= number;
     if (Math.abs(counter) > NUMBER_LIMIT) { 
-        counterInit(); 
+        initializeCounter(); 
     } else {
-        counterUpdate(numberElement);
+        updateCounter(numberElement);
     }
 }
 
-counterInit();
+// Initial counter setup
 
-// Button click events
+let counter;
+const NUMBER_LIMIT = 99; // After reaching the number limit, the counter will restart from 0
+
+// Page rendering
+
+renderPage();
+
+// Elements selectors
+
+const btnWrapper = document.querySelector('.btn-wrapper');
+const numberElement = document.querySelector('.number');
+
+// First counter initialization
+
+initializeCounter();
+
+// The Button Wrapper event listener - EVENT DELEGATION
 
 btnWrapper.addEventListener("click", function(e) {
     e.target.classList.contains("plus") ? modifyCounter(1) : 
     e.target.classList.contains("minus") ? modifyCounter(-1): 
-    e.target.classList.contains("reset") ? counterInit() :
+    e.target.classList.contains("reset") ? initializeCounter() :
     e.stopPropagation();
 });
